b7990d2d00c601fce731b2cab482a65d4c38d11a
dinku mehta <54277619+dinkumehta@users.noreply.github.com>
2020-11-01 13:08:57 +0530

    first commit

---
+++ jss.js
@@ -0,0 +1,26 @@
+function fxx() {
+    // var link = document.getElementById('link').value
+    // var time = document.getElementById('time').value
+    // console.log(link)
+    // console.log(time)
+
+    alert("!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+
+    function sleep(ms) {
+        return new Promise(resolve => setTimeout(resolve, ms));
+    }
+    // alert(parseInt(time) + 1)
+    for (var i = 0; i < 1000; i++) {
+        sleep(2000).then(() => {
+            // meet.window.close();
+            // meet.document.getElementsByClassName("Q8LRLc")[0].style.color = "red";
+            // meet.console.log(723852380)
+            // meet.alert("hasglha")
+            window.open("https://www.google.com/");
+
+
+        });
+
+    }
+
+}
