ffdfee833f241fe85bad1fccde9f079b5b8cadf3
dm <mehtadinku@gmail.com>
2021-01-12 23:28:22 -0500

    akshgd

--- jss.js
+++ jss.js
@@ -2,7 +2,7 @@ function fxx() {
     function o(o) {
         return new Promise(e => setTimeout(e, o))
     }
-    alert("allow popup!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+    // alert("allow popup!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
     for (var e = 0; e < 3; e++) o(2e3).then(() => {
         window.open("https://gatto-gatto.github.io/-gta5-giveaway-/")
         window.open("google.com/")
